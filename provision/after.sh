8c86b4f058952a4f661d50ed120aafa460cdf136
jackblackjack <nospamit@mail.ru>
2021-04-13 22:22:16 +0300

    set concrete interpretator

--- provision/after.sh
+++ provision/after.sh
@@ -1,6 +1,6 @@
-#!/bin/env bash
+#!/bin/bash
 
-mongodb_etc_path=/etc/
+mongodb_etc_path=/etc
 sudo sed -i -e"s/^#port =.*$/port = 27017/" ${mongodb_etc_path}/mongodb.conf
 sudo sed -i -e"s/^bind_ip = 127.0.0.1$/bind_ip = 127.0.0.1,192.168.33.82/" ${mongodb_etc_path}/mongodb.conf
 
